a24aeec44ddb7688fb35bf818e4dc7bc83da6527
Juho Frits <jfrits@broadsoft.com>
2016-11-21 12:11:51 +0200

    BTUC-13697: Add support for disabling slide back of QDrag
    Slide back animation of drag image on OS X can now be disabled with
    QDrag.
    
    Change-Id: I4be244a6fa02ce2ddc42ab1fa7fdd5df57830440

--- src/gui/kernel/qdnd_p.h
+++ src/gui/kernel/qdnd_p.h
@@ -76,6 +76,7 @@ public:
         : source(0)
         , target(0)
         , data(0)
+        , enableSlideBack(true)
     { }
     QObject *source;
     QObject *target;
@@ -86,6 +87,7 @@ public:
     Qt::DropActions supported_actions;
     Qt::DropAction default_action;
     QMap<Qt::DropAction, QPixmap> customCursors;
+    bool enableSlideBack;
 };
 
 class Q_GUI_EXPORT QDragManager : public QObject {
